7734874aeb9a35cc123538ee8e0b4358a03e0702
sso <sojeong7524@gmail.com>
2023-10-25 09:35:56 +0900

    ajax #5

--- ch08/src/main/java/ch08/controller/api/ApiController.java
+++ ch08/src/main/java/ch08/controller/api/ApiController.java
@@ -1,7 +1,9 @@
 package ch08.controller.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import ch08.dto.JsonResult;
@@ -10,27 +12,47 @@ import ch08.vo.GuestbookVo;
 @Controller
 @RequestMapping("/api")
 public class ApiController {
-
+	
 	@ResponseBody
-	@RequestMapping("/text")
+	@RequestMapping(value="/text", method=RequestMethod.GET)
 	public String text() {
 		return "Text 데이타";
 	}
-
+	
 	@ResponseBody
-	@RequestMapping("/html")
+	@RequestMapping(value="/html", method=RequestMethod.GET)
 	public String html() {
 		return "<h1>AJAX 연습</h1><p>HTML 데이터</p>";
 	}
 
 	@ResponseBody
-	@RequestMapping("/json")
+	@RequestMapping(value="/json", method=RequestMethod.GET)
 	public JsonResult json() {
 		GuestbookVo vo = new GuestbookVo();
 		vo.setNo(1L);
 		vo.setName("둘리");
 		vo.setContents("호이~~");
+		
+		return JsonResult.success(vo);
+	}
 
+	@ResponseBody
+	@RequestMapping(value="/post01", method=RequestMethod.POST)
+	public JsonResult post01(GuestbookVo vo) {
+		// service -> repository : db insert 성공한 후,
+		vo.setNo(1L);
+		vo.setPassword("");
+		
+		return JsonResult.success(vo);
+	}
+
+	@ResponseBody
+	@RequestMapping(value="/post02", method=RequestMethod.POST)
+	public JsonResult post02(@RequestBody GuestbookVo vo) {
+		// service -> repository : db insert 성공한 후,
+		vo.setNo(1L);
+		vo.setPassword("");
+		
 		return JsonResult.success(vo);
 	}
 }
--- ch08/src/main/webapp/WEB-INF/views/json04.jsp
+++ ch08/src/main/webapp/WEB-INF/views/json04.jsp
@@ -16,21 +16,21 @@ $(function(){
 		contents: '호이~'	
 	};
 	
-	
 	$('button').click(function(){
+		console.log(JSON.stringify(vo));
+		
 		$.ajax({
-			url: '${pageContext.request.contextPath }/api/post01',
+			url: '${pageContext.request.contextPath }/api/post02',
 			async: true,
 			type: 'post',
 			dataType: 'json',
-			contentType: 'application/x-www-form-urlencoded',
-			data: $.param(vo),
+			contentType: 'application/json',
+			data: JSON.stringify(vo),
 			success: function(response) {
 				if(response.result !== "success") {
 					console.error(response.message);
 					return;
 				}
-
 				var vo = response.data;
 				
 				var htmls = "";
