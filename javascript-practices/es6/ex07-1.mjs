1a94cbb15378b6902ac318ad2261283273a849ed
sso <sojeong7524@gmail.com>
2023-10-27 13:07:31 +0900

    es#4

--- javascript-practices/es6/ex07-1.mjs
+++ javascript-practices/es6/ex07-1.mjs
@@ -1,6 +1,7 @@
 /**
  * unamed export
  * default 키워드를 사용한다.
+ * 
  */
 
 // const add = function(a, b) {
@@ -11,3 +12,8 @@
 export default function(a, b) {
     return a + b;
 }
+
+// 주의) 에러: 이름없이 export를 하기 때문에 하나만 export 할 수 있다.
+// export default function(a, b) {
+//     return a - b;
+// }
