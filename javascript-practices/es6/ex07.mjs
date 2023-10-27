1f2ea3c22a6e951c1452858e70d8067e27b63f24
sso <sojeong7524@gmail.com>
2023-10-27 13:51:47 +0900

    async #1

--- javascript-practices/es6/ex07.mjs
+++ javascript-practices/es6/ex07.mjs
@@ -13,9 +13,13 @@ console.log(myObject.add(10, 20), myObject.subtract(10, 20));
 
 // 3.
 // 1) named 다수 객체 import 시 객체분해
-import {add, subtract} from  './ex07-3.mjs';
-console.log(add(10, 20), subtract(10, 20));
+import {subtract} from  './ex07-3.mjs';
+console.log(subtract(10, 20));
 // 2) named export는 import 대상이 다수가 될 수 있기 때문에 특정 이름을 지정할 수 없다.
 //    대신에 * as (이름) 를 사용할 수 있다.
 import * as m from  './ex07-3.mjs';
 console.log(m.add(10, 20), m.subtract(10, 20));
+
+// 4.
+import math, {add} from './ex07-4.mjs';
+console.log(add(10, 20), math.subtract(10, 20));
